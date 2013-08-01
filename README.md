92078d449160556f1232fa7041b2de333de090ac
Sasha Klein <sashafklein@gmail.com>
2013-08-01 16:13:13 -0700

    Initialized repo

---
+++ README.md
@@ -0,0 +1,5 @@
+**************
+** THE IDEA **
+**************
+
+A to-do list that automatically clears itself of tasks older than 3 days, makes it more difficult to keep tasks on the list, and blocks re-occurring tasks. The hope is to incentivize more actionable tasks and more actual doing. 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todone::Application.config.secret_token = '2c751fd6af46556a4779a41b49f2905dd4469719b2a17650126ea8bfe2275d85937c48d4c9c2a41d4b1a1b33f8fc735deb73cfabcb0b22f5ba6f1b6dbb193642'
