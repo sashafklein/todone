76dfe1be0911fb4258b8b696c6f9a07c77d8845e
Sasha Klein <sashafklein@gmail.com>
2013-08-05 12:56:41 -0700

    WIP

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,59 @@
+class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_user
+
+  def index
+    @items = @user.items
+  end
+
+  def show
+  end
+
+  # ItemsController.permits(:description)
+  def new
+    @item = Item.new
+  end
+
+  def edit
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to @item, notice: 'Item was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  # PATCH/PUT /items/1
+  def update
+    if @item.update(item_params)
+      redirect_to @item, notice: 'Item was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  # DELETE /items/1
+  def destroy
+    @item.destroy
+    redirect_to items_url, notice: 'Item was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def item_params
+      params[:item]
+    end
+end
